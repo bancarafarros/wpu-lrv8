a319acefb8fa8ac992312c213914b767b8deb16e
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2024-05-23 14:02:55 +0700

    finish 3 - struktur folder, routes, dan view

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Gonn",
+        "email" => "gonn@gmail.com",
+        "image" => "gonn.png"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('blog');
 });
